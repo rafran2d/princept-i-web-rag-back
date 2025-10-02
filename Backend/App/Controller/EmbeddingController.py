3eea8f91559c95fd599aba29fd74d93037652c56
Andrews <randrewsravalo@gmail.com>
2025-10-02 14:57:53 +0300

    feature/embedding(with_bacth_processing)

--- Backend/App/Controller/EmbeddingController.py
+++ Backend/App/Controller/EmbeddingController.py
@@ -1,19 +1,35 @@
 from App.Service.Embedding_service import (chunk_embedding, create_embedding)
-from App.Schema.EmbeddingShcema import (EmbeddingCreate,EmbeddingRead)
+from App.Schema.EmbeddingShcema import (EmbeddingRead)
 from App.Schema.DocumentChunkSchema import ChunkRead
-from typing import List
+from typing import List,Dict,Any
 from App.Exception.IngestionException import(
     SaveEmbeddingError,
     EmbeddingError
 )
+import asyncio
 
-async def embedding_management(chunk : ChunkRead) -> List[EmbeddingRead]:
+BATCH_SIZE = 10
+MAX_RETRIES = 1
+
+async def simple_embedding_process(chunk : ChunkRead) -> List[EmbeddingRead]:
+    for attempt in range(1,MAX_RETRIES + 2):
+        try:
+            chunk_output = await chunk_embedding(chunk)
+            embedding_output = await create_embedding(chunk_output)
+            
+            return embedding_output
+        
+        except (SaveEmbeddingError,EmbeddingError) as e:
+            if attempt <= MAX_RETRIES:
+                await asyncio.sleep(2)            
+            else:
+                raise e
+            
+async def batch_embedding_process(chunks: List[ChunkRead]) :
     try:
-        chunk_output = await chunk_embedding(chunk)
-        embedding_output = await create_embedding(chunk_output)
-        
-        return embedding_output
-    
-    except (SaveEmbeddingError,EmbeddingError) as e:
+        for i in range(0,len(chunks,BATCH_SIZE)):
+            batch = chunks[i:i + BATCH_SIZE]
+            tasks = [simple_embedding_process(chunk) for chunk in batch]
+            await asyncio.gather(*tasks, return_exceptions=True)
+    except (SaveEmbeddingError,EmbeddingRead) as e:
         raise e
-        
--- Backend/App/Route/ChatRoute.py
+++ Backend/App/Route/ChatRoute.py
@@ -4,18 +4,17 @@ from App.Controller.DocumentController import upload_documents as document_step
 from App.Schema.DocumentSchema import DocumentRead
 from App.Schema.DocumentChunkSchema import ChunkRead
 from App.Service.Document_service import (set_ready_document,set_document_failed)
-from App.Controller.EmbeddingController import embedding_management as embedding_step
+from App.Controller.EmbeddingController import batch_embedding_process 
 from typing import List,Dict,Any
 import uuid
-import logging
+
 
 chat_route = APIRouter(
     prefix = "/chat",
     tags=['chat']
 )
 
-#log the exception
-logger = logging.getLogger(__name__)
+
 
 @chat_route.post("{chat_id}/documents",response_model=DocumentRead)
 async def documemt_ingestion(chat_id : uuid.UUID) -> Dict[str,Any]:
@@ -23,8 +22,7 @@ async def documemt_ingestion(chat_id : uuid.UUID) -> Dict[str,Any]:
         documents_output : List[DocumentRead] = await document_step(chat_id)
         for document in documents_output:
             chunk_list : List[ChunkRead] = await chunk_step(document)
-            for chunk in chunk_list:
-                embedding_step(chunk)
+            batch_embedding_process(chunk_list)
             set_ready_document(document.id)
         return {
             "status_code" : 200,
@@ -32,5 +30,4 @@ async def documemt_ingestion(chat_id : uuid.UUID) -> Dict[str,Any]:
             "message" : "Documents upload completed"
         }
     except Exception as e:
-        logger.exception("erreur")#to log the exception
         raise HTTPException(status_code = 500, detail=f"Erreur serveur:{str(e)}") from e
